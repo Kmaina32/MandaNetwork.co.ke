4d03c7eb1e49e01fa47a378be516399b874d99f0
kairo kairo <GMaina424@gmail.com>
2025-11-02 08:52:43 +0000

    ## Error Type Console Error
    ## Error Message
    There should always be an

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -1,23 +1,8 @@
 
-import { Suspense } from 'react';
-import { LoadingAnimation } from '@/components/LoadingAnimation';
-
-function LoadingFallback() {
-    return (
-        <div className="flex h-screen items-center justify-center">
-            <LoadingAnimation />
-        </div>
-    );
-}
-
 export default function SignupLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-      <Suspense fallback={<LoadingFallback />}>
-        {children}
-      </Suspense>
-  );
+  return <>{children}</>;
 }
