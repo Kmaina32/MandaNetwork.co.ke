54580adb2df768c028f1cf6031c51a075c8306c3
kairo kairo <GMaina424@gmail.com>
2025-11-09 14:14:28 +0000

    add the metamask account form for admin payments page :

--- src/app/admin/blockchain/page.tsx
+++ src/app/admin/blockchain/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Footer } from "@/components/shared/Footer";
-import { ArrowLeft, Coins, Loader2, Wallet } from 'lucide-react';
+import { ArrowLeft, Coins, Loader2, Wallet, AlertCircle } from 'lucide-react';
 import { useAuth } from '@/hooks/use-auth';
 import { useRouter } from 'next/navigation';
 import { Icon } from '@iconify/react';
@@ -17,7 +17,6 @@ import { useToast } from '@/hooks/use-toast';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert';
-import { AlertCircle } from 'lucide-react';
 
 const contractAddressIsSet = MANDA_TOKEN_CONTRACT_ADDRESS !== '0x0000000000000000000000000000000000000000';
 
@@ -35,6 +34,9 @@ export default function AdminBlockchainPage() {
     useEffect(() => {
         if (!authLoading && !isSuperAdmin) {
             router.push('/admin');
+        }
+         if (typeof window.ethereum === 'undefined') {
+            setNoWallet(true);
         }
     }, [isSuperAdmin, authLoading, router]);
 
