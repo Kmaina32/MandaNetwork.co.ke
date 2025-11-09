6743f4080fd926064db1cfdca675194dc0b86651
kairo kairo <GMaina424@gmail.com>
2025-11-09 14:30:28 +0000

    here is my account address for the admin:0x04bc91B2d280A76623A8d9d76b94B

--- src/lib/blockchain/contracts.ts
+++ src/lib/blockchain/contracts.ts
@@ -1,4 +1,4 @@
 // This file contains the deployed addresses of your smart contracts.
 // Replace the placeholder address with your actual contract address after deployment.
 
-export const MANDA_TOKEN_CONTRACT_ADDRESS = "0x0000000000000000000000000000000000000000";
+export const MANDA_TOKEN_CONTRACT_ADDRESS = "0x04bc91B2d280A76623A8d9d76b94B60C2e525D49";
