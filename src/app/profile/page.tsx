06f6628061423381caab8a447cfaae30050fa860
kairo kairo <GMaina424@gmail.com>
2025-11-09 18:27:21 +0000

    ## Error Type Runtime ReferenceError
    ## Error Message
    useFieldArray is

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,7 +4,7 @@
 import { useEffect, useState, useRef } from 'react';
 import { useRouter, useParams, notFound } from 'next/navigation';
 import Link from 'next/link';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, Controller, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { v4 as uuidv4 } from 'uuid';
